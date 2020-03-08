5bf088298421396eaea4f5612c58782d2a00d6e1
Matt <amthoma5@ncsu.edu>
2020-03-08 08:13:06 -0500

    testing pycharm functionality using Git Hub 2

--- hello_world.py
+++
@@ -1,7 +0,0 @@
-
-
-print('Hello World')
-print('I opened pycharm')
-print('then i clicked "get from version control"')
-print('then i went to the Git Hub tab and selected to clone Hello-World from my Git Hub Repository')
-print('had to add the interpreter: file > settings > project hello-world > project interpreter')
