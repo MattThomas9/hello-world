5349005c86c2fa0df7165a69de5d2eca633a802a
Matt <48624622+mattthomas9@users.noreply.github.com>
2020-03-08 08:48:40 -0500

    updating hello_world.py in pycharm

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,3 @@
 
 
 print('hello world')
-
-print('test')
-
-print('second test via edit on Github.com')
-
-print('third test via edit on Github.com')
-
-print('fourth test via edit in pycharm!')
-
-print('fifth test testing conflicts between remote and local')
-
-print('sixth test via edit in pycharm')
